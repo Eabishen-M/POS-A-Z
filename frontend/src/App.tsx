abadc0e92383e1752f499415553942312df63c62
Eabishen <ebi4lbe@gmail.com>
2025-12-10 00:17:12 +0530

    inital setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-6xl font-black">Eazy Buy</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
